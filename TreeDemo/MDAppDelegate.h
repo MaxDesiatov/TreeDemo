9299f84717c5a3b1a85ac105f56f02e0fe1b61de
Max Desyatov <max.desyatov@gmail.com>
2012-11-08 19:17:54 +0200

    Initial Commit

---
+++ TreeDemo/MDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDAppDelegate.h
+//  TreeDemo
+//
+//  Created by Max Desyatov on 08/11/2012.
+//  Copyright (c) 2012 Max Desyatov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TreeDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TreeDemo
+//
+//  Created by Max Desyatov on 08/11/2012.
+//  Copyright (c) 2012 Max Desyatov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MDAppDelegate class]));
+    }
+}
